30d7eb680aec23b854874354063caa9dbb6deee4
Khaleel <khaleelk@yupptv.com>
2019-01-08 15:17:40 +0530

    Initial commit

---
+++ SampleSDKCOCO/SampleSDKCOCO.h
@@ -0,0 +1,19 @@
+//
+//  SampleSDKCOCO.h
+//  SampleSDKCOCO
+//
+//  Created by YuppTV on 08/01/19.
+//  Copyright © 2019 YuppTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleSDKCOCO.
+FOUNDATION_EXPORT double SampleSDKCOCOVersionNumber;
+
+//! Project version string for SampleSDKCOCO.
+FOUNDATION_EXPORT const unsigned char SampleSDKCOCOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSDKCOCO/PublicHeader.h>
+
+
